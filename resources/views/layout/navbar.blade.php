c801c2383ed37329692f2df3f3a98cbf103e34b8
Karan Sanjeev <yo@alphaman.me>
2022-03-06 13:57:25 +0530

    Made basic view layout

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar">
+    <div class="container">
+        <div class="navbar-brand">
+            <a href="{{ url('/') }}" class="navbar-item is-size-5 has-text-weight-semibold">
+                {{ config('app.name') }}
+            </a>
+        </div>
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                @auth
+                    {{-- Links for authenticated user --}}
+                @else
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <a href="" class="button is-link">
+                                Sign Up
+                            </a>
+                            <a href="" class="button is-dark">
+                                Log In
+                            </a>
+                        </div>
+                    </div>
+                @endauth
+            </div>
+        </div>
+    </div>
+</nav>
