799558bab0c4254512daec96c5e59574e54696b6
Karan Sanjeev <yo@alphaman.me>
2022-03-08 09:44:23 +0530

    Basic event linking @dashboard

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,16 +1,21 @@
 @extends('layout.app')
 
 @section('content')
-    <section class="section has-background-link">
+    <section class="section">
         <div class="columns">
-            <div class="column is-half">
-                <div class="box">
-
-                </div>
-            </div>
-            <div class="column is-half">
-                <div class="box">
-
+            <div class="column is-offset-3 is-half">
+                <div class="card">
+                    <div class="card-header">
+                        <p class="card-header-title">
+                            Events
+                        </p>
+                    </div>
+                    <div class="card-content">
+                        <div class="buttons">
+                            <a href="#" class="button is-light is-fullwidth">Create Event</a>
+                            <a href="#" class="button is-dark is-fullwidth">List Events</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
